cae953f0be5b283d5a00346bedc3edb5199578c5
Christian Vinther <81313481+Vinther07@users.noreply.github.com>
2021-04-03 16:47:09 +0200

    Create Find the smallest integer in the array.py

---
+++ Find the smallest integer in the array.py
@@ -0,0 +1,9 @@
+def find_smallest_int(arr):
+    # Code here
+    return(min(arr))
+  
+  # If this wasn't a code challenge I would include some test for the output asswell.
+  
+# def find_smallest_int(arr):
+#    # Code here
+#    print("Smallest integer in array is: ",min(arr))
